7c048c2b004153165dcb0098a7c313a4d671afa5
Ben P <109652329+clooster@users.noreply.github.com>
2022-12-07 07:04:36 -0800

    Update streamlit_app.py
    added fruit_list csv, dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
+import pandas
 
 streamlit.title("Menu")
 streamlit.header("Breakfast Menu")
 streamlit.text("Kale and eggs")
+
+fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(fruitlist)
