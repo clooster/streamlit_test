aa8f7e0e7ed37d7d3fda1556868e65febd9bab98
Ben P <109652329+clooster@users.noreply.github.com>
2022-12-07 07:08:00 -0800

    Update streamlit_app.py
    created fruit select

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Kale and eggs")
 
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick a fruit", list(fruit_list.index))
 streamlit.dataframe(fruit_list)
