2dcab7706f62795917dc894c1e989466a59f6b6e
Ben P <109652329+clooster@users.noreply.github.com>
2022-12-07 07:15:11 -0800

    Update streamlit_app.py
    added filtering function

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ streamlit.text("Kale and eggs")
 
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index("Fruit")
-streamlit.multiselect("Pick a fruit", list(fruit_list.index), ['Avocado','Strawberries'])
-streamlit.dataframe(fruit_list)
+fruit_selected = streamlit.multiselect("Pick a fruit", list(fruit_list.index), ['Avocado','Strawberries'])
+streamlit.dataframe(fruit_list[fruit_selected])
