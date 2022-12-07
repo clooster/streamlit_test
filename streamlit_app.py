5f9e4cbfeb3d1b808c2b19daff8b18371ac32fa4
Ben P <109652329+clooster@users.noreply.github.com>
2022-12-07 07:05:12 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Kale and eggs")
 
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(fruitlist)
+streamlit.dataframe(fruit_list)
